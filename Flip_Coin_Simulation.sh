e6d6724857ea921ce2a48d4f64bc61cb838db9b0
Your Name <you@example.com>
2021-07-26 14:27:18 +0530

    No of times heads and tails getting

--- Flip_Coin_Simulation.sh
+++ Flip_Coin_Simulation.sh
@@ -1,9 +1,35 @@
 echo "welcome to flip coin simulation problems"
 #!/bin/bash -x
-Result=$((RANDOM%2))
-if [[ ${Result} -eq 0 ]]; then
-    echo HEADS
-elif [[ ${Result} -eq 1 ]]; then
-    echo TAILS
-fi
+function flipCoin()
+{
+        randomCheck=$((RANDOM%2))
+        if [ $randomCheck -eq 0 ]
+        then
+                echo "HEAD"
+        else
+                echo "TAIL"
+        fi
+}
 
+declare -A singlets
+declare -A  winingCombination
+isHead="HEAD"
+read -p "Enter number of times to flip the coin " n
+for (( i=0;i<n;i++ ))
+do
+        result="$( flipCoin )"
+        singlets[$i]=$result;
+        if [ "$result" == "$isHead" ]
+        then
+                heads=$((heads+1))
+        else
+                tails=$((tails+1))
+        fi
+done
+echo "Generated Singlets:"
+echo ${singlets[@]};
+echo "Percentage of Singlets";
+echo "Percentage of Heads:= $((heads*100/n))";
+echo "Percentage of Tails:= $((tails*100/n))";
+winingCombination[H]=$((heads*100/n))
+winingCombination[T]=$((tails*100/n))
